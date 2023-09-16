6b262fb3dd42d85da8daf18001c5f5a3cbf7466b
Matheus Segre <matheussegre.soares@gmail.com>
2023-09-15 22:53:32 -0300

    Responsivo do Login e Cadastro

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const btnLogin = document.getElementById('btn-login');
+const btnClose = document.querySelector('.js-close');
+const overlayLogin = document.querySelector('.js-login');
+
+const overlayCad = document.querySelector('.js-cadastro');
+const btnCad = document.getElementById('js-btn-cad');
+const btnCloseCad = document.querySelector('.js-close-cad');
+
+function openMenuLogin() {
+    overlayLogin.classList.toggle('active');
+}
+
+function openMenuCad() {
+    overlayCad.classList.toggle('active');
+    overlayLogin.classList.remove('active');
+}
+
+btnLogin.addEventListener('click', openMenuLogin);
+
+btnClose.addEventListener('click', openMenuLogin);
+
+btnCad.addEventListener('click', openMenuCad);
+
+btnCloseCad.addEventListener('click', openMenuCad);
